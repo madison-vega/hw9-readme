40751f53e75c42921139703b4c73118a6b11109f
Madison Vega <madison.r.vega@gmail.com>
2021-03-28 20:46:40 -0500

    added more functionality to license function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,13 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === 'None') {
-    return ''
-  } 
-  
-}
 
-// TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license === 'None') {
     return ''
-  } 
-}
+  } else {
+    return `The license for this project is ${data.license}`
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -52,8 +42,7 @@ ${data.credit}
 
 ## License
 
-${renderLicenseLink (data.license)}
-${renderLicenseSections (data.license)}
+${renderLicenseSection (data.license)}
 
 
 ## Tests
